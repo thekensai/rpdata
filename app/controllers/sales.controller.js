eec269b6f7dfe5a97e3e62922e666d52bda70e0d
j w <j w>
2017-02-06 23:10:14 +1100

    add sales template

--- app/controllers/sales.controller.js
+++ app/controllers/sales.controller.js
@@ -9,6 +9,7 @@ module.exports = {
   host = 'https://search-sandbox-api.corelogic.asia',
   path = '/search/au/property/geo/radius/lastSale?radius=100&lat=-28.008176&lon=153.428172&beds=2-4&pTypes=HOUSE&price=500000-700000&sort=beds,desc&sort=price,asc&page=0&size=5';
 
+
   var request = require('request');
   //console.log(global.access_token);
   request({
@@ -21,16 +22,14 @@ module.exports = {
   }, function (err, response, body) {
   		//console.log('error', err);
   		if (response && response.headers['app_messages']) {
-      		console.log(req.params);
+      		//console.log(req.params);
           console.log(response.headers['app_messages'] ); 
-      		res.writeHead(404);
+      		res.writeHead(500);
       		res.end();
   		}
       	else {
-      		//console.log(response);
-      		//console.log(body);
-      		//res.writeHead(200, {'Content-Type': 'image/png'});
-      		res.send(response);
+      		var json = JSON.parse(body);
+      		res.render('pages/sales', json["_embedded"]);
       	}
     });
 
